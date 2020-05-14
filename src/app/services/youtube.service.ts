09f7d7aa7b31396b6336c2e1449c494abb1690ff
Alex <omareyes-96@hotmail.com>
2020-05-13 22:51:47 -0500

    crear estructura de componentes y servicios

---
+++ src/app/services/youtube.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root' //permite el que el servico pueda ser utilizado de manera global en la aplicaion
+})
+export class YoutubeService {
+
+  constructor() { }
+}
