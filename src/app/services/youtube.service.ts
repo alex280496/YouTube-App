ab4acc1703caee308c09b791a25d53a6d8689af2
Alex <omareyes-96@hotmail.com>
2020-05-13 23:09:30 -0500

    Probar Api mediante Postman

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -6,5 +6,10 @@ import { HttpClient } from '@angular/common/http';
 })
 export class YoutubeService {
 
+    private youtubeUrl='https://www.googleapis.com/youtube/v3';
+    private apikey='AIzaSyD7nZHI5rJ3scfwqeTw78GQaQhmt1KOhIY';
+    private playlist='UUuaPTYj15JSkETGnEseaFFg';
+    private nextPageToken='';
+
   constructor(private http:HttpClient) { }
 }
